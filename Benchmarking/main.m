eba98abd288613b1a3ba1b01530d071f9d741f72
VladiMihaylenko <vxmihaylenko@gmail.com>
2015-01-21 21:00:06 +0300

    Initial Commit

---
+++ Benchmarking/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Benchmarking
+//
+//  Created by Vlad Mihaylenko on 21/01/15.
+//  Copyright (c) 2015 Vlad Mihaylenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
